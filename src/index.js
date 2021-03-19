8155a571c8d0460b8bed798ec2f27eb68bfb0ae4
ClintFix <clint.fix@gmail.com>
2021-03-19 09:43:31 -0600

    SmurfList component connected

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
-import { reducer } from './reducers'
+import reducer from './reducers'
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
 
