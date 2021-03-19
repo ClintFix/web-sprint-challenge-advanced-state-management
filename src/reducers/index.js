a313a860dbc1d48daa58fb98d3ee50f07dfee371
ClintFix <clint.fix@gmail.com>
2021-03-19 09:21:50 -0600

    actions setup

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,18 @@
 
 export const initialState = {
+    smurfs: [],
+    isLoading: false,
+    error: '',
 }
 
-const reducer = ()=>{
+const reducer = ( state = initialState, action ) => {
+    switch (action.type) {
+        //CASES GO HERE
+        
+        default: 
+            return state;
+    }
+
 }
 
 export default reducer;
